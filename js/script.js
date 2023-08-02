690de6a349b344f164aac20c3e003fc0fd2f8f9d
Wilson-Juniorr <115658132+Wilson-Juniorr@users.noreply.github.com>
2023-08-02 16:51:32 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, notANumber } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANamber = notANumber(weight) || notANumber(height)
+
+    if (weightOrHeightIsNotANamber) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+    const result = calculateIMC(weight, height)
+    
+    displayResultMessage(result)
+
+    inputWeight.value = ""
+    inputHeight.value = ""
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
